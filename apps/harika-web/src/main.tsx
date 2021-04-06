ec6664d23b6d484b3496a62a096d01bfa1ae6be2
Sergey Popov <quolpr@gmail.com>
2021-04-06 17:20:23 +0300

    Fix service worker location

--- apps/harika-web/src/main.tsx
+++ apps/harika-web/src/main.tsx
@@ -5,7 +5,7 @@ import { App } from '@harika/harika-ui';
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('/serviceWorker.js');
   });
 }
 
