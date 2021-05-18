f3a27ddfa467d4c2d601c550cf405c723d522065
Sergey Popov <quolpr@gmail.com>
2021-05-19 00:42:32 +0300

    Service worker loading

--- packages/web-app/src/App.tsx
+++ packages/web-app/src/App.tsx
@@ -40,6 +40,13 @@ if (import.meta.env.MODE === 'production') {
   });
 }
 
+if (import.meta.env.MODE === 'production' && 'serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 Modal.setAppElement('body');
 
 const queryClient = new QueryClient();
