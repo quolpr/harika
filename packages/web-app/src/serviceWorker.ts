12d4583ee3c16da5a4dede7e5c9f8fef4bb38e0e
Sergey Popov <quolpr@gmail.com>
2021-05-22 12:08:03 +0300

    Better serviceWorker

--- packages/web-app/src/serviceWorker.ts
+++ packages/web-app/src/serviceWorker.ts
@@ -1,36 +1,12 @@
 import { ExpirationPlugin } from 'workbox-expiration';
-import { registerRoute, setCatchHandler } from 'workbox-routing';
-import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);
-
-// Cache page navigations (html) with a Network First strategy
-registerRoute(
-  // Check to see if the request is a navigation to a new page
-  ({ request }) => request.mode === 'navigate',
-  // Use a Network First caching strategy
-  new NetworkFirst({
-    // Put all cached files in a cache named 'pages'
-    cacheName: 'pages',
-    plugins: [
-      // Ensure that only requests that result in a 200 status are cached
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  }),
-);
-
-registerRoute(
-  ({ request }) =>
-    request.destination === 'script' || request.destination === 'style',
-  new StaleWhileRevalidate({}),
-);
 
 // Cache Google Fonts with a stale-while-revalidate strategy, with
 // a maximum number of entries.
@@ -43,13 +19,3 @@ registerRoute(
     plugins: [new ExpirationPlugin({ maxEntries: 20 })],
   }),
 );
-
-// Catch routing errors, like if the user is offline
-setCatchHandler(async ({ event }): Promise<any> => {
-  // Return the precached offline page if a document is being requested
-  if (event.request.destination === 'document') {
-    return matchPrecache('/index.html');
-  }
-
-  return Response.error();
-});
