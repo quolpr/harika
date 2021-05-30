06bbdb2a97ab440a15d1d183692022397af44a52
Sergey Popov <quolpr@gmail.com>
2021-05-30 13:35:07 +0300

    Fix checkbox ticking

--- packages/web-app/src/components/NoteBlock/TokensRenderer.tsx
+++ packages/web-app/src/components/NoteBlock/TokensRenderer.tsx
@@ -37,8 +37,9 @@ const RefRenderer = observer(
             type="checkbox"
             checked={token.content === 'DONE'}
             onChange={handleTodoToggle}
+            data-not-editable
           />
-          <svg className="checkbox__tick" viewBox="0 0 20 20">
+          <svg className="checkbox__tick" viewBox="0 0 20 20" data-not-editable>
             <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
           </svg>
         </label>
