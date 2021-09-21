67ab9530f249deb3a454c13c2bc0a3ffa3929386
Sergey Popov <quolpr@gmail.com>
2021-09-21 17:02:22 +0300

    Fix google fonts cache

--- packages/web-app/src/serviceWorker.ts
+++ packages/web-app/src/serviceWorker.ts
@@ -1,22 +1,40 @@
 import { ExpirationPlugin } from 'workbox-expiration';
 import { registerRoute } from 'workbox-routing';
-import { NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies';
+import {
+  CacheFirst,
+  NetworkOnly,
+  StaleWhileRevalidate,
+} from 'workbox-strategies';
 import { precacheAndRoute, PrecacheFallbackPlugin } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache Google Fonts with a stale-while-revalidate strategy, with
-// a maximum number of entries.
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
 registerRoute(
-  ({ url }) =>
-    url.origin === 'https://fonts.googleapis.com' ||
-    url.origin === 'https://fonts.gstatic.com',
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
   new StaleWhileRevalidate({
-    cacheName: 'google-fonts',
-    plugins: [new ExpirationPlugin({ maxEntries: 20 })],
+    cacheName: 'google-fonts-stylesheets',
+  }),
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.gstatic.com',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
   }),
 );
 
