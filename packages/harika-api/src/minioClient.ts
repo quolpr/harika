38688ba308227853797c76901642abf3bf12709c
Sergey Popov <quolpr@gmail.com>
2022-04-11 23:42:22 +0300

    Fix minio config

--- packages/harika-api/src/minioClient.ts
+++ packages/harika-api/src/minioClient.ts
@@ -2,8 +2,7 @@ import { Client } from 'minio';
 
 export const minioClient = new Client({
   endPoint: process.env.S3_ENDPOINT,
-  port: parseInt(process.env.S3_PORT, 10) || 3000,
-  useSSL: false,
+  port: parseInt(process.env.S3_PORT, 10) || 443,
   accessKey: process.env.S3_ACCESS_TOKEN,
   secretKey: process.env.S3_SECRET_KEY,
 });
